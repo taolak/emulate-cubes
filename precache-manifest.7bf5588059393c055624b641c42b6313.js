6038b5cfbd68801487bd495a737b34f33a842a2c
T.K <taolak@TKs-MacBook-Pro.local>
2020-01-19 10:14:18 -0600

    Updates

---
+++ precache-manifest.7bf5588059393c055624b641c42b6313.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1d534532aa2ce1da313ef55be90a87",
+    "url": "/emulate-cubes/index.html"
+  },
+  {
+    "revision": "1a8748ae983fbc8ce984",
+    "url": "/emulate-cubes/static/css/main.d5c5affc.chunk.css"
+  },
+  {
+    "revision": "878a4436dfc5ef82aff1",
+    "url": "/emulate-cubes/static/js/2.7451d95c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/emulate-cubes/static/js/2.7451d95c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a8748ae983fbc8ce984",
+    "url": "/emulate-cubes/static/js/main.46dc768b.chunk.js"
+  },
+  {
+    "revision": "9cd0c162c2e2d513bf2a",
+    "url": "/emulate-cubes/static/js/runtime-main.7682ab21.js"
+  }
+]);
